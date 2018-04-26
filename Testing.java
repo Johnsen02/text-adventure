b576f09b86ef1da6ddc6c4142455f17f6ca37851
grant <ghb11344@gmail.com>
2018-04-26 09:42:27 -0700

    Fix
    Printing out properly

--- Testing.java
+++ Testing.java
@@ -13,8 +13,11 @@ for(int i = 0 ; i < 5; i++){
 	for(int j = 0 ; j < 5 ; j++){
 		rand = (int)(Math.random() * 100);
 		arr[i][j] = rand;
-								}
-							}
+		System.out.print (arr[i][j]);	
+	}
+
+	System.out.println();
+}
 //print out array
 System.out.println(arr[0][0]);
 
