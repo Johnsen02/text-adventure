08305beed2faa30f2b8b46904d825f396429752c
grant <ghb11344@gmail.com>
2018-05-03 09:12:01 -0700

    Loopin
    Arithemtic Sequnces

---
+++ Loop.java
@@ -0,0 +1,15 @@
+public class Loop {
+
+	public static void main(String[] args){
+		int n = 5;
+		int product = n;
+	for(int i = 1 ; i < n; i++ ){
+		
+		product = product *(n-i);
+		
+		}
+			System.out.println(product);
+		
+	
+	}
+}
